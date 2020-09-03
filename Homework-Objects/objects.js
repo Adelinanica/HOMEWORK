e2bd97c20941b6b225b6694be17ec5b820a0a3aa
Adelinanica <69160568+Adelinanica@users.noreply.github.com>
2020-09-03 12:33:36 +0300

    Add objects to Homework

---
+++ Homework-Objects/objects.js
@@ -0,0 +1,17 @@
+var animal= {
+    type: "dog",
+    name: "Milo",
+    gut: ["rice", "carrot",],
+    gutadd: function (gutadd) {
+        this.gut = this.gut + " " + gutadd;
+        console.log(gutadd);
+        
+    }
+};
+
+console.log(animal);
+
+console.log( "before :", animal.gut);
+animal.gutadd(["turkey","potato","broccoli"]);
+console.log("full belly:", animal.gut);
+
