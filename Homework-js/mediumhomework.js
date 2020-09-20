627236ab11213a9b53fb263a8a8e52a38326c0b2
Adelinanica <69160568+Adelinanica@users.noreply.github.com>
2020-09-20 18:37:52 +0300

    add this in homework

--- Homework-js/mediumhomework.js
+++ Homework-js/mediumhomework.js
@@ -2,7 +2,7 @@ var today = new Date ();
 var hourNow = today.getHours();
 var message;
 
-if (hourNow < 12  ) {
+if (hourNow <12  ) {
     message = "Good Morning!";
 }  else if (hourNow > 18 ) {
     message = "Good Evening!"; 
