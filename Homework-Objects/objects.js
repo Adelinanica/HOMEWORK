188b3f4307ea77a78fa3788e324331fd111871cf
Adelinanica <69160568+Adelinanica@users.noreply.github.com>
2020-10-04 13:32:46 +0300

    add changes

--- Homework-Objects/objects.js
+++ Homework-Objects/objects.js
@@ -12,5 +12,14 @@ console.log(animal);
 
 console.log("before :", animal.gut);
 // here you need to add an individual food, not a list, please use the gutadd function 3 times to add the 3 items
-animal.gutadd(["turkey", "potato", "broccoli"]);
+animal.gutadd("turkey");
 console.log("full belly:", animal.gut);
+
+animal.gutadd("potato");
+console.log("full belly",animal.gut);
+
+animal.gutadd("broccoli");
+console.log("full", animal.gut);
+
+
+
